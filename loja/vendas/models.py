80a1eca5d408bfb89752c238476c1e821f5e98ef
Gabriel <83376188+Gebrel07@users.noreply.github.com>
2024-06-16 11:47:37 -0300

    adicionado modelo Vendedor
    - adicionado modelo Pessoa (absctract = True para que não seja criada tabela no banco de dados)
    
    - adicionada migração para Vendedor

--- loja/vendas/models.py
+++ loja/vendas/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=255, null=False)
+    cpf = models.CharField(max_length=11, null=False, unique=True)
+    data_nascimento = models.DateField(null=False)
+
+    class Meta:
+        abstract = True
+
+
+class Vendedor(Pessoa):
+    matricula = models.CharField(max_length=255, null=False, unique=True)
+    data_contratacao = models.DateField(null=False)
+
+    def __str__(self) -> str:
+        return f"id: {self.id}, nome: {self.nome}, matricula: {self.matricula}"
