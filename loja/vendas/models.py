ed44a96517373654a620638f4f311690ab5d1610
Gabriel <83376188+Gebrel07@users.noreply.github.com>
2024-06-16 14:48:42 -0300

    adicionado modelo Venda
    - criada migração

--- loja/vendas/models.py
+++ loja/vendas/models.py
@@ -51,3 +51,19 @@ class Produto(models.Model):
     def __str__(self) -> str:
         return f"id: {self.id}, nome: {self.nome}, preco: {self.preco}, \
             cor: {self.cor}, tamanho: {self.tamanho}"
+
+
+class Venda(models.Model):
+    data_venda = models.DateField(null=False)
+    quantidade = models.IntegerField(default=1, null=False)
+    comissao = models.FloatField(default=0, null=False)
+    vendedor = models.ForeignKey(
+        to=Vendedor, on_delete=models.SET_NULL, null=True
+    )
+    cliente = models.ForeignKey(
+        to=Cliente, on_delete=models.SET_NULL, null=True
+    )
+    produto = models.ForeignKey(to=Produto, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"id: {self.id}, data_venda: {self.data_venda}, quantidade: {self.quantidade}"
