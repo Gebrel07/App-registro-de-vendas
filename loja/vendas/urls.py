3797390b34c1c93bc3d8325ec1708812689dacce
Joao Dacoregio <oinaagua6@gmail.com>
2024-06-17 08:34:26 -0300

    Conectando o home com vendedores para teste

--- loja/vendas/urls.py
+++ loja/vendas/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('vendedores/',views.listar_vendedores, name="listar_vendedores"),
     path('criar/', views.criar_vendedor, name='criar_vendedor'),  # Criar vendedor
     path('editar/<int:vendedor_id>/', views.editar_vendedor, name='editar_vendedor'),  # Editar vendedor
 ]
