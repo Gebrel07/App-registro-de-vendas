194ff12766874becf182bceae4cebb23ee13492c
Gabriel <83376188+Gebrel07@users.noreply.github.com>
2024-06-17 22:18:42 -0300

    formatado vendas/urls.py

--- loja/vendas/urls.py
+++ loja/vendas/urls.py
@@ -1,13 +1,24 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
-    path('vendas/',views.page_vendas,name='page_vendas'),
-    path('vendedores/',views.listar_vendedores, name="listar_vendedores"),
-    path('vendedores/criar/', views.criar_vendedor, name='criar_vendedor'),  # Criar vendedor
-    path('vendedores/editar/<int:vendedor_id>/', views.editar_vendedor, name='editar_vendedor'),  # Editar vendedor
-    path('clientes/',views.listar_clientes , name="listar_clientes"),
-    path('clientes/criar/',views.criar_cliente, name='criar_cliente'),
-    path('clientes/editar/<int:cliente_id>/', views.editar_cliente, name='editar_cliente'),
+    path("", views.home, name="home"),
+    path("vendas/", views.page_vendas, name="page_vendas"),
+    path("vendedores/", views.listar_vendedores, name="listar_vendedores"),
+    path(
+        "vendedores/criar/", views.criar_vendedor, name="criar_vendedor"
+    ),  # Criar vendedor
+    path(
+        "vendedores/editar/<int:vendedor_id>/",
+        views.editar_vendedor,
+        name="editar_vendedor",
+    ),  # Editar vendedor
+    path("clientes/", views.listar_clientes, name="listar_clientes"),
+    path("clientes/criar/", views.criar_cliente, name="criar_cliente"),
+    path(
+        "clientes/editar/<int:cliente_id>/",
+        views.editar_cliente,
+        name="editar_cliente",
+    ),
 ]
