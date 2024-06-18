d2ea489fb2990e2d597156927162f97a1310d39f
Gabriel <83376188+Gebrel07@users.noreply.github.com>
2024-06-17 22:14:42 -0300

    formatado forms.py

--- loja/vendas/forms.py
+++ loja/vendas/forms.py
@@ -1,22 +1,37 @@
 from django import forms
-from .models import Vendedor,Cliente,Endereco
+
+from .models import Cliente, Endereco, Vendedor
+
 
 class VendedorForm(forms.ModelForm):
     class Meta:
         model = Vendedor
-        fields = ['id', 'nome', 'cpf', 'data_nascimento', 'matricula', 'data_contratacao']
+        fields = [
+            "id",
+            "nome",
+            "cpf",
+            "data_nascimento",
+            "matricula",
+            "data_contratacao",
+        ]
         widgets = {
-            'data_nascimento': forms.DateInput(attrs={'type': 'date'}),
-            'data_contratacao': forms.DateInput(attrs={'type': 'date'}),
+            "data_nascimento": forms.DateInput(attrs={"type": "date"}),
+            "data_contratacao": forms.DateInput(attrs={"type": "date"}),
         }
+
 
 class EnderecoForm(forms.ModelForm):
     class Meta:
         model = Endereco
-        fields = ['rua', 'cidade', 'estado', 'cep']  # Adicione os campos do modelo Endereco que deseja exibir
+        fields = [
+            "rua",
+            "cidade",
+            "estado",
+            "cep",
+        ]  # Adicione os campos do modelo Endereco que deseja exibir
+
 
 class ClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
-        fields = ['nome', 'cpf', 'data_nascimento', 'email', 'telefone']
- 
+        fields = ["nome", "cpf", "data_nascimento", "email", "telefone"]
