9fc6e8e3b71d236a0050df7d8383242c1f55a976
Joao Dacoregio <oinaagua6@gmail.com>
2024-06-19 07:53:39 -0300

    organizando e comentando url.py

--- loja/vendas/urls.py
+++ loja/vendas/urls.py
@@ -6,33 +6,24 @@ from .vendedor.views import *
 from .cliente.views import *
 
 urlpatterns = [
+    # Home
     path("", views.home, name="home"),
+
+    # Vendas
     path("vendas/", views.page_vendas, name="page_vendas"),
+    path("vendas/selecionar_cliente", selecionar_cliente, name="selecionar_cliente"),
+    path("vendas/selecionar_vendedor", selecionar_vendedor, name="selecionar_vendedor"),
+
+    # Vendedores
     path("vendedores/", listar_vendedores, name="listar_vendedores"),
-    path(
-        "vendedores/criar/", criar_vendedor, name="criar_vendedor"
-    ),  # Criar vendedor
-    path(
-        "vendedores/editar/<int:vendedor_id>/",
-        editar_vendedor,
-        name="editar_vendedor",
-    ),  # Editar vendedor
+    path("vendedores/criar/", criar_vendedor, name="criar_vendedor"),  # Criar vendedor
+    path("vendedores/editar/<int:vendedor_id>/", editar_vendedor, name="editar_vendedor"),  # Editar vendedor
+
+    # Clientes
     path("clientes/", listar_clientes, name="listar_clientes"),
     path("clientes/criar/", criar_cliente, name="criar_cliente"),
-    path(
-        "clientes/editar/<int:cliente_id>/",
-        editar_cliente,
-        name="editar_cliente",
-    ),
-    path(
-        "vendas/selecionar_cliente",
-         selecionar_cliente,
-        name="selecionar_cliente",
-    ),
-    path(
-        "vendas/selecionar_vendedor",
-        selecionar_vendedor,
-        name="selecionar_vendedor",
-    ),
+    path("clientes/editar/<int:cliente_id>/", editar_cliente, name="editar_cliente"),
+
+    # Produtos
     path("produtos/criar", criar_produto, name="criar_produto"),
 ]
