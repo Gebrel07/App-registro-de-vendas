08354537d23ad3a81caca8ed3a7c11331fb21193
Gabriel <83376188+Gebrel07@users.noreply.github.com>
2024-06-18 21:43:46 -0300

    atualizada view criar_produto

--- loja/vendas/views.py
+++ loja/vendas/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpRequest
 from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib import messages
 
 from .forms import ClienteForm, EnderecoForm, ProdutoForm, VendedorForm
-from .models import Cliente, Endereco, Vendedor
+from .models import Cliente, Endereco, Produto, Vendedor
 
 
 def home(request: HttpRequest) -> render:
@@ -128,8 +129,29 @@ def selecionar_vendedor(request: HttpRequest) -> render:
 
 def criar_produto(request: HttpRequest) -> render:
     if request.method == "POST":
-        # TODO: finalizar rota
-        raise NotImplementedError()
+        form = ProdutoForm(request.POST)
+        if form.is_valid():
+            try:
+                novo_produto = Produto(
+                    nome=form.cleaned_data["nome"],
+                    preco=form.cleaned_data["preco"],
+                    cor=form.cleaned_data["cor"],
+                    tamanho=form.cleaned_data["tamanho"],
+                    referencia=form.cleaned_data["referencia"],
+                )
+                novo_produto.save()
+                messages.success(
+                    request=request,
+                    message="Produto criado com sucesso!",
+                )
+            except Exception:
+                # TODO: logar erros
+                messages.error(
+                    request=request,
+                    message="Erro ao criar produto",
+                )
+            finally:
+                return redirect("criar_produto")
     else:
         form = ProdutoForm()
         return render(
