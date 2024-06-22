7065b4858e12c946ee7eef6d8076949c9582aaa5
Gabriel <83376188+Gebrel07@users.noreply.github.com>
2024-06-22 12:54:48 -0300

    renomeado submoludo templates/vendas
    - referencias atualizadas em views

--- loja/vendas/venda/views.py
+++ loja/vendas/venda/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 
 def page_vendas(request: HttpRequest) -> render:
-    return render(request, "vendas/vendas/vendas.html")
+    return render(request, "vendas/venda/vendas.html")
