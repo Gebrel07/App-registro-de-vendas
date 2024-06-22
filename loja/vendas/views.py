65b8bf5e9de0156b7e2f1b6a42687700960b2887
Gabriel <83376188+Gebrel07@users.noreply.github.com>
2024-06-22 12:45:56 -0300

    movido template home

--- loja/vendas/views.py
+++ loja/vendas/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 
 def home(request: HttpRequest) -> render:
-    return render(request, "vendas/pages/home.html")
+    return render(request, "vendas/home.html")
