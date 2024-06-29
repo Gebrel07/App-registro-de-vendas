c0b64feda6f4b52bded8b6ee7701b29762369cfa
Joaodacoregio <125033983+Joaodacoregio@users.noreply.github.com>
2024-06-29 11:49:11 -0300

    tooltip basico

--- loja/vendas/static/vendas/calculos.js
+++ loja/vendas/static/vendas/calculos.js
@@ -108,3 +108,10 @@ function calcularTotalDesconto(itemVenda) {
 function limparVenda(){
     localStorage.clear();
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+});
