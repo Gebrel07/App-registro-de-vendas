1efa7a53afddbb9702dbc059e1bbad5a4875e590
Joaodacoregio <125033983+Joaodacoregio@users.noreply.github.com>
2024-07-01 16:36:20 -0300

    fix em bug quando envia a venda com desconto proporcional

--- loja/vendas/static/vendas/calculos.js
+++ loja/vendas/static/vendas/calculos.js
@@ -52,7 +52,7 @@ function handleDescontoTotalChange(listaItens, totalPreco) {
             const descontoProporcional = (totalPreco > 0) ? (precoProduto * itemVenda.quantidade / totalPreco) : 0;
             const descontoItem = (descontoProporcional * (descontoTotalBruto));
 
-            itemVenda.desconto = Number(descontoItem);
+            itemVenda.desconto = Number(descontoItem).toFixed(2);
             calcularTotal(itemVenda);
             // Atualizar a interface com o novo desconto
             document.getElementById(`desconto${itemVenda.produtoId}Input`).value = descontoItem.toFixed(2);
