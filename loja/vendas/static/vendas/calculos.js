b761b6c21535efb11bcf511817f2af732b34d287
Joao Dacoregio <oinaagua6@gmail.com>
2024-07-01 15:27:55 -0300

    botão para enviar a venda para o db

--- loja/vendas/static/vendas/calculos.js
+++ loja/vendas/static/vendas/calculos.js
@@ -94,6 +94,104 @@ function limparVenda(){
     localStorage.clear();
 }
 
+function obterDataAtual() {
+    var dataAtual = new Date();
+
+    var ano = dataAtual.getFullYear(); // Obtém o ano com 4 dígitos
+    var mes = ('0' + (dataAtual.getMonth() + 1)).slice(-2); // Obtém o mês com zero à esquerda (01 a 12)
+    var dia = ('0' + dataAtual.getDate()).slice(-2); // Obtém o dia com zero à esquerda (01 a 31)
+
+    // Formata a data no formato yyyy-mm-dd
+    var dataFormatada = ano + '-' + mes + '-' + dia;
+
+    return dataFormatada;
+}
+
+
+function formatarProdutoId(listaItens) {
+    // Percorre cada item na lista
+    listaItens.forEach(item => {
+        // Verifica se o item possui a chave 'produtoId'
+        if ('produtoId' in item) {
+            // Renomeia a chave 'produtoId' para 'produto'
+            item.produto = item.produtoId;
+            delete item.produtoId; // Remove a chave antiga 'produtoId' se necessário
+        }
+    });
+
+    return listaItens;
+}
+
+async function enviarVenda() {
+    // Obter cliente
+    const cliente = localStorage.getItem("clienteId");
+
+    // Obter vendedor 
+    const vendedor = localStorage.getItem("vendedorId");
+
+    // Obter a lista de itens com produtoId == produto
+    const listaItens = localStorage.getItem('listaItens');
+
+
+    // Validar dados essenciais
+    if (!cliente || !vendedor || !listaItens) {
+        console.error('Dados essenciais estão faltando');
+        return;
+    }
+
+    // Obter a data no momento do click do botão
+    const dataHora = obterDataAtual();
+
+    // Obter o tipo de pagamento
+    const tipoPagamento = document.getElementById("parcelasSelect").value;
+
+
+
+
+
+
+    // Montar tudo em um JSON
+    const venda = {
+        vendedor: vendedor,
+        cliente: cliente,
+        data_venda: dataHora,
+        tipo_pgto:  tipoPagamento > 1 ? 2 : 1,
+        parcelas_pgto: tipoPagamento > 1 ? tipoPagamento - 1 : 0,
+        itens: formatarProdutoId(JSON.parse(listaItens))
+    };
+
+    try {
+        // Enviar a requisição POST
+        const response = await fetch('/vendas/criar/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(venda)
+        });
+
+        // Verificar a resposta
+        const contentType = response.headers.get('content-type');
+        if (response.ok) {
+            if (contentType && contentType.indexOf('application/json') !== -1) {
+                const data = await response.json();
+                console.log('Venda criada com sucesso:', data);
+            } else {
+                console.log('Resposta não é JSON:', await response.text());
+            }
+        } else {
+            if (contentType && contentType.indexOf('application/json') !== -1) {
+                const errorData = await response.json();
+                console.error('Erro ao criar venda:', errorData);
+            } else {
+                console.error('Erro ao criar venda. Resposta não é JSON:', await response.text());
+            }
+        }
+    } catch (error) {
+        console.error('Erro na requisição:', error);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
