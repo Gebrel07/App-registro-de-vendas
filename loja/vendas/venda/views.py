b87be73bbf361f8cda8bc352053d3bd45f05f408
Gabriel <83376188+Gebrel07@users.noreply.github.com>
2024-07-13 19:56:23 -0300

    removidas views antigas
    - urls atualizadas

--- loja/vendas/venda/views.py
+++ loja/vendas/venda/views.py
@@ -1,16 +1,11 @@
 from django.http import HttpRequest
 from django.shortcuts import render
 
-from ..cliente.models import Cliente
-from ..produto.models import Produto
-from ..vendedor.models import Vendedor
 from .models import Venda
 
 
 def nova_venda(request: HttpRequest):
-    return render(
-        request=request, template_name="vendas/venda/nova_venda.html"
-    )
+    return render(request=request, template_name="vendas/venda/nova_venda.html")
 
 
 def listar_vendas(request: HttpRequest):
@@ -19,31 +14,4 @@ def listar_vendas(request: HttpRequest):
         request=request,
         template_name="vendas/venda/listar_vendas.html",
         context={"vendas": vendas},
-    )
-
-
-def selecionar_cliente(request: HttpRequest) -> render:
-    clientes = Cliente.objects.all()
-    return render(
-        request,
-        "vendas/cliente/selecionar_cliente.html",
-        {"clientes": clientes},
-    )
-
-
-def selecionar_vendedor(request: HttpRequest) -> render:
-    vendedores = Vendedor.objects.all()
-    return render(
-        request,
-        "vendas/vendedor/selecionar_vendedor.html",
-        {"vendedores": vendedores},
-    )
-
-
-def selecionar_produto(request: HttpRequest) -> render:
-    produtos = Produto.objects.all()
-    return render(
-        request,
-        "vendas/produto/selecionar_produto.html",
-        {"produtos": produtos},
     )
